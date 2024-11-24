6e4b3d9573933b17964ef226a28fac4ba8e4e0b0
Joker066 <alexdad063@gmail.com>
2024-11-24 13:37:30 +0100

    create /documents

--- files.py
+++ files.py
@@ -1,20 +1,20 @@
 """
-Loads the word list from words.txt
+Loads the word list from documents/words.txt
 """
 def load_words():
     words = []
-    with open("words.txt", "r") as f:
+    with open("documents/words.txt", "r") as f:
         for word in f.readlines():
             voc, meaning, weight = word.split('_')
             words.append({"voc": voc, "meaning": meaning, "weight": int(weight[:-1])})
     return words
 
 """
-Updates the current word list to words.txt
+Updates the current word list to documents/words.txt
 """
 def update_words(words: list):
     words.sort(key=lambda x: x["voc"])
-    with open("words.txt", "w") as f:
+    with open("documents/words.txt", "w") as f:
         for word in words:
             try:
                 f.write(f"{word["voc"]}_{word["meaning"]}_{word["weight"]}\n")
@@ -22,11 +22,11 @@ def update_words(words: list):
                 continue
 
 """
-Load the records form records.txt
+Load the records form documents/records.txt
 """
 def load_records():
     records = []
-    with open("records.txt", "r") as f:
+    with open("documents/records.txt", "r") as f:
         for record in f.readlines():
             if record == "\n":
                 continue
@@ -39,10 +39,10 @@ def load_records():
     return records
 
 """
-Updates the current records to records.txt
+Updates the current records to documents/records.txt
 """
 def update_records(records: list):
-    with open("records.txt", "w") as f:
+    with open("documents/records.txt", "w") as f:
         for record in records:
             f.write(f"{record["accuracy"]}")
             for word in record["wrong_words"]:
